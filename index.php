f8ca6775d42423e3c0e49b7b768fa7f2d6729e15
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 18:14:06 -0500

    Update index.php
    titulo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Work Scout</title>
+<title>TuObrero.com</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" href="css/customNoty.css">
 <link rel="stylesheet" href="css/noty.css">
