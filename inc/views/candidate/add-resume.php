0e9b23d0f1703485bdbbbd5bbaf203ded8e39465
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 20:11:23 -0500

    Update add-resume.php
    curriculum

--- inc/views/candidate/add-resume.php
+++ inc/views/candidate/add-resume.php
@@ -1,10 +1,10 @@
-<!-- Titlebar
+<!-- Barra de Título
 ================================================== -->
 <div id="titlebar" class="single submit-page">
     <div class="container">
 
         <div class="sixteen columns">
-            <h2><i class="fa fa-plus-circle"></i> Add Resume</h2>
+            <h2><i class="fa fa-plus-circle"></i> Agregar Currículum</h2>
         </div>
 
     </div>
@@ -25,130 +25,115 @@
 </script>
 <?php
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-            $user_id = $_SESSION['user_id'];
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $title = $_POST['title'];
-            $location = $_POST['location'];
-            $description = mysqli_real_escape_string($con, $_POST['description']);
-            $url = $_POST['url'];
-            $education = mysqli_real_escape_string($con, $_POST['education']);
-            $experience = mysqli_real_escape_string($con, $_POST['experience']);
-            $skills = $_POST['skills'];
-            $date_posted = date_format(new DateTime(), 'Y-m-d H:i:s');
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $user_id = $_SESSION['user_id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $title = $_POST['title'];
+    $location = $_POST['location'];
+    $description = mysqli_real_escape_string($con, $_POST['description']);
+    $url = $_POST['url'];
+    $education = mysqli_real_escape_string($con, $_POST['education']);
+    $experience = mysqli_real_escape_string($con, $_POST['experience']);
+    $skills = $_POST['skills'];
+    $date_posted = date_format(new DateTime(), 'Y-m-d H:i:s');
 
-//            for($j=0; $j < count($_FILES["image"]['name']); $j++){
-//                echo $_FILES["image"]['tmp_name'][$j] . "\n";
-//                move_uploaded_file($_FILES["upload"]["tmp_name"][$j],"upload/" . $_FILES["upload"]["name"][$j]);
-//            }
+    $sql = "INSERT INTO resumes (user_id, name, email, professional_title, location, content, url, education, experience, skills, date_posted) VALUES ($user_id, '$name', '$email', '$title', '$location', '$description', '$url', '$education', '$experience', '$skills', '$date_posted')";
 
-            $sql = "INSERT INTO resumes (user_id, name, email, professional_title, location, content, url, education, experience, skills, date_posted) VALUES ($user_id, '$name', '$email', '$title', '$location', '$description', '$url', '$education', '$experience', '$skills', '$date_posted')";
-//            echo $sql;
-            if (!empty($_FILES["image"]['tmp_name'])){
-                $image = $user_id.'_'.$_FILES["image"]['name'];
-                $image_tmp = $_FILES["image"]['tmp_name'];
-                move_uploaded_file($image_tmp, "images/candidate/" . $image);
-                $link_img = "images/candidate/" . $image;
-            }
-            else {
-                $link_img = "images/candidate/avatar-placeholder.png";
-            }
-            $sql = "INSERT INTO resumes (user_id, name, email, professional_title, location, content, url, education, experience, link_img, skills, date_posted) VALUES ($user_id, '$name', '$email', '$title', '$location', '$description', '$url', '$education', '$experience', '$link_img', '$skills', '$date_posted')";
-            $con->query($sql);
-            if(($con->affected_rows)>0) echo "<script>showNoty('success', 'Add resume successfully!');</script>";
-            else echo "<script>showNoty('error', 'Error! Add resume failed!');</script>";
-        }
+    if (!empty($_FILES["image"]['tmp_name'])){
+        $image = $user_id.'_'.$_FILES["image"]['name'];
+        $image_tmp = $_FILES["image"]['tmp_name'];
+        move_uploaded_file($image_tmp, "images/candidate/" . $image);
+        $link_img = "images/candidate/" . $image;
+    }
+    else {
+        $link_img = "images/candidate/avatar-placeholder.png";
+    }
+    $sql = "INSERT INTO resumes (user_id, name, email, professional_title, location, content, url, education, experience, link_img, skills, date_posted) VALUES ($user_id, '$name', '$email', '$title', '$location', '$description', '$url', '$education', '$experience', '$link_img', '$skills', '$date_posted')";
+    $con->query($sql);
+    if(($con->affected_rows)>0) echo "<script>showNoty('success', 'Currículum agregado exitosamente');</script>";
+    else echo "<script>showNoty('error', '¡Error! Fallo al agregar el currículum');</script>";
+}
 
 ?>
-<!-- Content
+<!-- Contenido
 ================================================== -->
 <div class="container">
 
-    <!-- Submit Page -->
+    <!-- Página de Envío -->
     <div class="sixteen columns">
         <div class="submit-page">
 
             <form method="post" enctype="multipart/form-data">
-                <!-- Name -->
+                <!-- Nombre -->
                 <div class="form">
-                    <h5>Your Name</h5>
-                    <input name="name" class="search-field" type="text" placeholder="Your full name" value=""/>
+                    <h5>Tu Nombre</h5>
+                    <input name="name" class="search-field" type="text" placeholder="Tu nombre completo" value=""/>
                 </div>
 
-                <!-- Email -->
+                <!-- Correo Electrónico -->
                 <div class="form">
-                    <h5>Your Email</h5>
-                    <input name="email" class="search-field" type="text" placeholder="mail@example.com" value=""/>
+                    <h5>Tu Correo Electrónico</h5>
+                    <input name="email" class="search-field" type="text" placeholder="correo@example.com" value=""/>
                 </div>
 
-                <!-- Title -->
+                <!-- Título Profesional -->
                 <div class="form">
-                    <h5>Professional Title</h5>
-                    <input name="title" class="search-field" type="text" placeholder="e.g. Web Developer" value=""/>
+                    <h5>Título Profesional</h5>
+                    <input name="title" class="search-field" type="text" placeholder="Ej. Desarrollador Web" value=""/>
                 </div>
 
-                <!-- Location -->
+                <!-- Ubicación -->
                 <div class="form">
-                    <h5>Location</h5>
-                    <input name="location" class="search-field" type="text" placeholder="e.g. London, UK" value=""/>
+                    <h5>Ubicación</h5>
+                    <input name="location" class="search-field" type="text" placeholder="Ej. Londres, Reino Unido" value=""/>
                 </div>
 
-                <!-- Logo -->
+                <!-- Foto -->
                 <div class="form">
-                    <h5>Photo <span>(optional)</span></h5>
+                    <h5>Foto <span>(opcional)</span></h5>
                     <label class="upload-btn">
                         <input name="image" type="file" accept="image/*"/>
-                        <i class="fa fa-upload"></i> Browse
+                        <i class="fa fa-upload"></i> Explorar
                     </label>
-                    <span class="fake-input">No file selected</span>
+                    <span class="fake-input">Ningún archivo seleccionado</span>
                 </div>
 
-                <!-- Description -->
+                <!-- Contenido del Currículum -->
                 <div class="form">
-                    <h5>Resume Content</h5>
+                    <h5>Contenido del Currículum</h5>
                     <textarea name="description" class="WYSIWYG" cols="40" rows="3" id="summary" spellcheck="true"></textarea>
                 </div>
 
-
-                <!-- Add URL -->
+                <!-- Agregar URL -->
                 <div class="form with-line">
-                    <h5>URL <span>(optional)</span></h5>
-                    <input name="url" class="search-field" type="text" placeholder="e.g. example.com" value=""/>
+                    <h5>URL <span>(opcional)</span></h5>
+                    <input name="url" class="search-field" type="text" placeholder="Ej. ejemplo.com" value=""/>
                 </div>
 
-
-                <!-- Education -->
+                <!-- Educación -->
                 <div class="form">
-                    <h5>Education <span>(optional)</span></h5>
-
-                        <!-- Add Education -->
-                            <textarea name="education" class="search-field" placeholder="School Name" cols="30" rows="5"></textarea>
-
+                    <h5>Educación <span>(opcional)</span></h5>
+                    <textarea name="education" class="search-field" placeholder="Nombre de la Escuela" cols="30" rows="5"></textarea>
                 </div>
 
-
-                <!-- Experience  -->
+                <!-- Experiencia -->
                 <div class="form with-line">
-                    <h5>Experience <span>(optional)</span></h5>
+                    <h5>Experiencia <span>(opcional)</span></h5>
                     <div class="form-inside">
-
-                        <!-- Add Experience -->
-                            <textarea name="experience" class="search-field" placeholder="Employer" cols="30" rows="5"></textarea>
-
+                        <textarea name="experience" class="search-field" placeholder="Empleador" cols="30" rows="5"></textarea>
                     </div>
                 </div>
                 
-                    <!-- Skills -->
+                <!-- Habilidades -->
                 <div class="form">
-                    <h5>Skills </span></h5>
-                    <input name="skills" class="search-field" type="text" placeholder="e.g. PHP, Social Media, Management" value=""/>
-                    <p class="note">Comma separate tags, such as required skills or technologies, for this job.</p>
+                    <h5>Habilidades </h5>
+                    <input name="skills" class="search-field" type="text" placeholder="Ej. PHP, Redes Sociales, Gestión" value=""/>
+                    <p class="note">Separa las etiquetas con comas, como habilidades o tecnologías requeridas para este trabajo.</p>
                 </div>
 
                 <div class="divider margin-top-0 padding-reset"></div>
-<!--                <a href="#" class="button big margin-top-5">Preview <i class="fa fa-arrow-circle-right"></i></a>-->
-                <input type="submit" class="button big" name="login" value="Add Resume" />
+                <input type="submit" class="button big" name="login" value="Agregar Currículum" />
             </form>
         </div>
     </div>
@@ -157,14 +142,13 @@
 <script>
     $(document).ready(function(){
         $('input[type=file]').change(function(){
-//            console.log($(this).val());//C:\fakepath\0013.jpg
             var filename = $(this).val();
             var lastIndex = filename.lastIndexOf("\\");
-             if (lastIndex >= 0) {
-                 filename = filename.substring(lastIndex + 1);
-              }
+            if (lastIndex >= 0) {
+                filename = filename.substring(lastIndex + 1);
+            }
             if (filename!="") $(".fake-input").html(filename);
-            else $(".fake-input").html('No file selected');
+            else $(".fake-input").html('Ningún archivo seleccionado');
         });
     });
 </script>
