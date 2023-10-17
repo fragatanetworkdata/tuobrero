164289838cefc49e03cdc3a042e180d3dc9c6775
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 20:07:14 -0500

    Update login.php

--- inc/views/account/login.php
+++ inc/views/account/login.php
@@ -1,15 +1,15 @@
-<!-- Titlebar
+<!-- Barra de Título
 ================================================== -->
 <div id="titlebar" class="single">
     <div class="container">
 
         <div class="sixteen columns">
-            <h2>My Account</h2>
+            <h2>Mi Cuenta</h2>
             <nav id="breadcrumbs">
                 <ul>
-                    <li>You are here:</li>
-                    <li><a href="#">Home</a></li>
-                    <li>My Account</li>
+                    <li>Estás aquí:</li>
+                    <li><a href="#">Inicio</a></li>
+                    <li>Mi Cuenta</li>
                 </ul>
             </nav>
         </div>
@@ -17,8 +17,7 @@
     </div>
 </div>
 
-
-<!-- Content
+<!-- Contenido
 ================================================== -->
 
 <?php
@@ -37,49 +36,45 @@
 
             header("location: index.php"); 
         } else {
-            echo "<div class='error'><h3>Username or Password is invalid</h3></div>";
+            echo "<div class='error'><h3>El nombre de usuario o la contraseña son inválidos</h3></div>";
 
         }
     }
-    
-    
-
 ?>
 
-<!-- Container -->
+<!-- Contenedor -->
 <div class="container">
 
     <div class="my-account">
 
-
         <div class="tabs-container">
-            <!-- Login -->
+            <!-- Inicio de Sesión -->
             <div class="tab-content">
                 <form method="post" class="login">
 
                     <p class="form-row form-row-wide">
-                        <label for="username">Username
+                        <label for="username">Nombre de Usuario
                             <i class="ln ln-icon-Male"></i>
                             <input type="text" class="input-text" name="username" id="username" value="" autofocus required/>
                         </label>
                     </p>
 
                     <p class="form-row form-row-wide">
-                        <label for="password">Password
+                        <label for="password">Contraseña
                             <i class="ln ln-icon-Lock-2"></i>
                             <input class="input-text" type="password" name="password" id="password" required/>
                         </label>
                     </p>
 
                     <p class="form-row">
-                        <input type="submit" class="button border fw margin-top-10" name="login" value="Login" />
+                        <input type="submit" class="button border fw margin-top-10" name="login" value="Iniciar Sesión" />
 
                         <label for="rememberme" class="rememberme">
-                            <input name="rememberme" type="checkbox" id="rememberme" value="forever" /> Remember Me</label>
+                            <input name="rememberme" type="checkbox" id="rememberme" value="forever" /> Recordarme</label>
                     </p>
 
                     <p class="lost_password">
-                        <a href="#" >Lost Your Password?</a>
+                        <a href="#" >¿Olvidaste Tu Contraseña?</a>
                     </p>
 
                 </form>
