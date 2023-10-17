95eeaf23e40beb9133686008a3b02a2c97ae0e69
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 20:49:34 -0500

    Update shortcodes.php

--- inc/views/page/shortcodes.php
+++ inc/views/page/shortcodes.php
@@ -1,183 +1,164 @@
-<!-- Titlebar
-================================================== -->
+<!-- Barra de Título -->
 <div id="titlebar" class="single">
     <div class="container">
-
         <div class="sixteen columns">
-            <h2>Pricing Tables</h2>
+            <h2>Tablas de Precios</h2>
             <nav id="breadcrumbs">
                 <ul>
-                    <li>You are here:</li>
-                    <li><a href="#">Home</a></li>
-                    <li>Pricing Tables</li>
+                    <li>Estás aquí:</li>
+                    <li><a href="#">Inicio</a></li>
+                    <li>Tablas de Precios</li>
                 </ul>
             </nav>
         </div>
-
     </div>
 </div>
 
-
-
-<!-- Pricing Tables
-================================================== -->
-
-<!-- Container / Start -->
+<!-- Tablas de Precios -->
 <div class="container">
-
     <div class="sixteen columns">
         <div class="notification notice margin-bottom-30">
-            <p>Below are examples of four and three columns of pricing tables, but you can easily have as many columns as you want.</p>
+            <p>A continuación, se muestran ejemplos de tablas de precios de cuatro y tres columnas, pero puedes tener tantas columnas como desees.</p>
         </div>
     </div>
 
-
-    <!-- Container / Start -->
+    <!-- Tres Tablas -->
     <div class="container">
-
         <div class="sixteen columns">
-            <h3 class="margin-bottom-20">Three Tables</h3>
+            <h3 class="margin-bottom-20">Tres Tablas</h3>
         </div>
 
         <!-- Plan #1 -->
         <div class="plan color-1 one-third column">
             <div class="plan-price">
-                <h3>Start Up</h3>
+                <h3>Inicio</h3>
                 <span class="plan-currency">$</span>
                 <span class="value">19</span>
-
             </div>
             <div class="plan-features">
                 <ul>
-                    <li>One Time Fee</li>
-                    <li>This Plan Includes 1 Job</li>
-                    <li>Non-Highlighted Post</li>
-                    <li>Posted For 30 Days</li>
+                    <li>Tarifa Única</li>
+                    <li>Este Plan Incluye 1 Empleo</li>
+                    <li>Publicación no Destacada</li>
+                    <li>Publicado por 30 Días</li>
                 </ul>
-                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
             </div>
         </div>
 
         <!-- Plan #2 -->
         <div class="plan color-2 one-third column">
-            <div class="plan-price">
-                <h3>Company</h3>
+            <div class "plan-price">
+                <h3>Empresa</h3>
                 <span class="plan-currency">$</span>
                 <span class="value">59</span>
             </div>
             <div class="plan-features">
                 <ul>
-                    <li>One Time Fee</li>
-                    <li>This Plan Includes 2 Jobs</li>
-                    <li>Highlighted Job Post</li>
-                    <li>Posted For 60 Days</li>
+                    <li>Tarifa Única</li>
+                    <li>Este Plan Incluye 2 Empleos</li>
+                    <li>Publicación Destacada de Empleo</li>
+                    <li>Publicado por 60 Días</li>
                 </ul>
-                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
             </div>
         </div>
 
         <!-- Plan #3 -->
         <div class="plan color-1 one-third column">
             <div class="plan-price">
-                <h3>Enterprise</h3>
+                <h3>Empresa</h3>
                 <span class="plan-currency">$</span>
                 <span class="value">99</span>
             </div>
             <div class="plan-features">
                 <ul>
-                    <li>One Time Fee</li>
-                    <li>This Plan Includes 4 Jobs</li>
-                    <li>2 Highlighted Job Posts</li>
-                    <li>Posted For 90 Days</li>
+                    <li>Tarifa Única</li>
+                    <li>Este Plan Incluye 4 Empleos</li>
+                    <li>2 Publicaciones Destacadas de Empleo</li>
+                    <li>Publicado por 90 Días</li>
                 </ul>
-                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+                <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
             </div>
         </div>
-
     </div>
-    <!-- Container / End -->
 
     <br>
     <br>
 
     <div class="sixteen columns">
-        <h3 class="margin-bottom-20">Four Tables</h3>
+        <h3 class="margin-bottom-20">Cuatro Tablas</h3>
     </div>
 
     <!-- Plan #1 -->
     <div class="plan color-1 four columns">
         <div class="plan-price">
-            <h3>Start Up</h3>
+            <h3>Inicio</h3>
             <span class="plan-currency">$</span>
             <span class="value">19</span>
-
         </div>
         <div class="plan-features">
             <ul>
-                <li>One Time Fee</li>
-                <li>This Plan Includes 1 Job</li>
-                <li>Non-Highlighted Post</li>
-                <li>Posted For 30 Days</li>
+                <li>Tarifa Única</li>
+                <li>Este Plan Incluye 1 Empleo</li>
+                <li>Publicación no Destacada</li>
+                <li>Publicado por 30 Días</li>
             </ul>
-            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
         </div>
     </div>
 
     <!-- Plan #2 -->
     <div class="plan color-1 four columns">
         <div class="plan-price">
-            <h3>Company</h3>
+            <h3>Empresa</h3>
             <span class="plan-currency">$</span>
             <span class="value">59</span>
         </div>
         <div class="plan-features">
             <ul>
-                <li>One Time Fee</li>
-                <li>This Plan Includes 2 Jobs</li>
-                <li>Highlighted Job Post</li>
-                <li>Posted For 60 Days</li>
+                <li>Tarifa Única</li>
+                <li>Este Plan Incluye 2 Empleos</li>
+                <li>Publicación Destacada de Empleo</li>
+                <li>Publicado por 60 Días</li>
             </ul>
-            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
         </div>
     </div>
 
     <!-- Plan #3 -->
     <div class="plan color-2 four columns">
         <div class="plan-price">
-            <h3>Enterprise</h3>
+            <h3>Empresa</h3>
             <span class="plan-currency">$</span>
             <span class="value">99</span>
         </div>
         <div class="plan-features">
             <ul>
-                <li>One Time Fee</li>
-                <li>This Plan Includes 4 Jobs</li>
-                <li>2 Highlighted Job Posts</li>
-                <li>Posted For 90 Days</li>
+                <li>Tarifa Única</li>
+                <li>Este Plan Incluye 4 Empleos</li>
+                <li>2 Publicaciones Destacadas de Empleo</li>
+                <li>Publicado por 90 Días</li>
             </ul>
-            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
         </div>
     </div>
 
     <!-- Plan #4 -->
     <div class="plan color-1 four columns">
         <div class="plan-price">
-            <h3>Extended</h3>
+            <h3>Extendido</h3>
             <span class="plan-currency">$</span>
             <span class="value">199</span>
         </div>
         <div class="plan-features">
             <ul>
-                <li>One Time Fee</li>
-                <li>This Plan Includes 10 Jobs</li>
-                <li>5 Highlighted Job Posts</li>
-                <li>Posted For 180 Days</li>
+                <li>Tarifa Única</li>
+                <li>Este Plan Incluye 10 Empleos</li>
+                <li>5 Publicaciones Destacadas de Empleo</li>
+                <li>Publicado por 180 Días</li>
             </ul>
-            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+            <a class="button" href="#"><i class="fa fa-shopping-cart"></i> Añadir al Carrito</a>
         </div>
     </div>
-
 </div>
-<!-- Container / End -->
-
-
