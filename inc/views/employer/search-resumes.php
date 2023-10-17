df711bf9672ffc23fe6d10483a967fb46254d22b
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 20:34:42 -0500

    Update search-resumes.php

--- inc/views/employer/search-resumes.php
+++ inc/views/employer/search-resumes.php
@@ -2,9 +2,7 @@
     .resumes-list li, .resumes-list li:last-child {
         border: none;
     }
-
 </style>
-
 
 <?php
     $query = $_POST['query'];
@@ -14,31 +12,23 @@
     $row_cnt = $result->num_rows;
 ?>
 
-
-
-<!-- Titlebar
-================================================== -->
+<!-- Titlebar -->
 <div id="titlebar">
     <div class="container">
         <div class="ten columns">
-            <span>We've found <?php echo $row_cnt ?> <?php echo ($row_cnt==1)?"resume":"resumes" ?>  matching</span>
+            <span>Hemos encontrado <?php echo $row_cnt ?> <?php echo ($row_cnt == 1) ? "currículum" : "currículums" ?> que coinciden</span>
         </div>
-
-
     </div>
 </div>
 
-
-<!-- Content
-================================================== -->
+<!-- Contenido -->
 <div class="container">
-    <!-- Recent Jobs -->
+    <!-- Currículums Recientes -->
     <div class="eleven columns">
         <div class="padding-right">
 
             <ul class="resumes-list">
                 <?php
-
                 while($row = $result->fetch_assoc()) {
                     $skills = explode(',', $row['skills']);
                     echo '<li><a href="?view=resume-page&resume_id='.$row['resume_id'].'">
@@ -47,7 +37,6 @@
                             <h4>'.$row['name'].' <span>'.$row['professional_title'].'</span></h4>
                             <span><i class="fa fa-map-marker"></i> '.$row['location'].'</span>
                             <span><i class="fa fa-envelope"></i> '.$row['email'].'</span>    
-
                             <div class="skills">';
 
                     foreach($skills as $skill){
@@ -55,22 +44,14 @@
                     }        
                     echo '</div>
                             <div class="clearfix"></div>
-
                         </div>
                     </a>
                     <div class="clearfix"></div>
                 </li>';
                 }
-                
                 ?>    
-                
-     
             </ul>
             <div class="clearfix"></div>
-
-        
         </div>
     </div>
-
-
 </div>
