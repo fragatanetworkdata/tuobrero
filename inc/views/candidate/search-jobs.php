9d038c720a11b00fd98de55a31d764fe86f94883
fragatanetworkdata <148160526+fragatanetworkdata@users.noreply.github.com>
2023-10-16 20:22:31 -0500

    Update search-jobs.php

--- inc/views/candidate/search-jobs.php
+++ inc/views/candidate/search-jobs.php
@@ -2,9 +2,7 @@
     .job-list li, .job-list li:last-child {
         border: none;
     }
-
 </style>
-
 
 <?php
     $query = $_POST['query'];
@@ -14,61 +12,41 @@
     $row_cnt = $result->num_rows;
 ?>
 
-
-<!-- Titlebar
+<!-- Barra de título
 ================================================== -->
 <div id="titlebar">
     <div class="container">
         <div class="ten columns">
-            <h2>We found <?php echo $row_cnt ?> <?php echo ($row_cnt==1)?"job":"jobs" ?> matching</h2>
-
+            <h2>Hemos encontrado <?php echo $row_cnt ?> <?php echo ($row_cnt==1)?"trabajo":"trabajos" ?> que coinciden</h2>
         </div>
-
-
     </div>
 </div>
 
-
-
-<!-- Content
+<!-- Contenido
 ================================================== -->
 <div class="container">
-    <!-- Recent Jobs -->
+    <!-- Trabajos recientes -->
     <div class="eleven columns">
-    <div class="padding-right">
-
-        <ul class="job-list full">
-            
-            <?php
-
-            while($row = $result->fetch_assoc()) {
-                echo '<li><a href="?view=job-page&job_id='.$row['job_id'].'">
-                    <img src="http://www.vasterad.com/themes/workscout/images/job-list-logo-05.png" alt="">
-                    <div class="job-list-content">
-                        <h4>'.$row['title'].' <span class="'.$row['job_type'].'">'.$row['job_type'].'</span></h4>
-                        <div class="job-icons">
-                            <span><i class="fa fa-briefcase"></i> '.$row['company'].'</span>
-                            <span><i class="fa fa-map-marker"></i> '.$row['location'].'</span>
-                            <span><i class="fa fa-money"></i> '.$row['rate'].'</span>
+        <div class="padding-right">
+            <ul class="job-list full">
+                <?php
+                while($row = $result->fetch_assoc()) {
+                    echo '<li><a href="?view=job-page&job_id='.$row['job_id'].'">
+                        <img src="http://www.vasterad.com/themes/workscout/images/job-list-logo-05.png" alt="">
+                        <div class="job-list-content">
+                            <h4>'.$row['title'].' <span class="'.$row['job_type'].'">'.$row['job_type'].'</span></h4>
+                            <div class="job-icons">
+                                <span><i class="fa fa-briefcase"></i> '.$row['company'].'</span>
+                                <span><i class="fa fa-map-marker"></i> '.$row['location'].'</span>
+                                <span><i class="fa fa-money"></i> '.$row['rate'].'</span>
+                            </div>
                         </div>
-                        
-                    </div>
-                    </a>
-                    <div class="clearfix"></div>
-                </li>';
-            }
-
-            ?>    
-            
-        </ul>
-
-        
-
-
+                        </a>
+                        <div class="clearfix"></div>
+                    </li>';
+                }
+                ?>
+            </ul>
+        </div>
     </div>
-    </div>
-
-
-
-
 </div>
